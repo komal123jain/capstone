47af73ce698bbf5435512028247a955ca45f4e3c
komal samdariya <koml17jain@gmail.com>
2020-11-30 12:10:05 +0530

    commit

--- account/views.py
+++ account/views.py
@@ -1,21 +1,56 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def login(request):
     if request.method == 'POST':
-        messages.error(request, 'Typing Error')
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are successfully logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Typing Error')
-        return redirect('register')
+
+        # GET form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # check if password match
+        if password == password2:
+            # check Username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is available')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'The Email is already registered')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(
+                        username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+                    auth.login(request, user)
+                    messages.success(request, 'You are logged in')
+                    return redirect('index')
+        else:
+            messages.error(request, 'Password does not match')
     else:
         return render(request, 'accounts/register.html')
 
